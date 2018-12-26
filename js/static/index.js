75827a78dc19b31d7a0b0af38c881c5d69cea74d
Rainbow <rainbow_weic@163.com>
2018-12-26 18:11:20 +0800

    添加首页详情页以及视频控制台的重写

--- js/static/index.js
+++ js/static/index.js
@@ -131,4 +131,173 @@ $(function(){
     //将当前窗口的内容区滚动高度改为0，即顶部
     $("html,body").animate({scrollTop:0},"fast");
 });
+
+//视频播放
+
+const setMedia = function(video, scale = 0.8) {
+    // 设置poster属性：（非本地视频资源会有跨域截图问题）
+    video.addEventListener('loadeddata', function(e) {
+        // 拿到图片
+        let canvas = document.createElement('canvas');
+        canvas.width = video.videoWidth * scale;
+        canvas.height = video.videoHeight * scale;
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+        let src = canvas.toDataURL('/images/vadio.png');
+        // 显示在dom，测试用
+        (function(flag = true) {
+            if (!flag) {return;}
+            let img = document.createElement('img');
+            img.src = src;
+            document.body.appendChild(img);
+        })(0);
+        // 设置属性
+        video.setAttribute('poster', src);
+    });
+
+}
+var all_oVid = document.getElementsByClassName("player");
+// console.log(all_oVid);
+for(var i = 0; i<all_oVid.length; i++){
+  oVid = all_oVid[i];
+  vides(oVid);
+}
+// console.log(all_oVid.length);
+function vides(oVid){
+  var oVideo = oVid.children[0];  //拿到video
+  var oCtl = oVid.children[1]
+  var oUl = oCtl.children;  //拿到所有li
+
+
+  var oUlWidth = parseInt(oUl[2].offsetWidth)-2;
+
+  var maxTime;
+  var thisTime;
+  var oSp = oUl[2].children[0];
+//   console.log(parseInt(oUl[3].style.left));
+  var oLeft = parseInt(oUl[2].offsetLeft)+parseInt(oVid.offsetLeft)+parseInt(oCtl.offsetLeft);
+  var oSchSp = parseInt(oVid.offsetLeft);
+
+
+
+  oVid.onmouseover = function(){
+          oCtl.style.opacity=0.8;
+  }
+  oVid.onmouseout = function(){
+      oCtl.style.opacity=0;
+  }
+
+  oUl[0].onclick = function(){
+      if(oVideo.paused){
+          oVideo.play();
+          oUl[0].innerHTML = "暂停";
+      }else{
+          oVideo.pause();
+          oUl[0].innerHTML = "播放";
+          oVid.onmouseover=null;
+          oVid.onmouseout =null;
+      }
+
+  }
+  oVideo.onclick = function(){
+      if(oVideo.paused){
+          oVideo.play();
+          oUl[0].innerHTML = "暂停";
+      }else{
+          oVideo.pause();
+          oUl[0].innerHTML = "播放";
+          oVid.onmouseover=null;
+          oVid.onmouseout =null;
+      }
+
+  }
+  var oDateOvideo = function(time){
+      time = ""+time;
+      time = time.length<2?0+time:time;
+      return time;
+  }
+  oVideo.onloadedmetadata = function(){
+      maxTime = oVideo.duration;
+      var h = oDateOvideo(Math.floor(maxTime/3600));      //时
+      var m = oDateOvideo(Math.floor(maxTime%3600/60));   //分
+      var s = oDateOvideo(Math.floor(maxTime%3600%60));   //秒
+      oUl[4].innerHTML = h+":"+m+":"+s;
+  }
+  function date(){
+      thisTime = oVideo.currentTime;
+      var h = oDateOvideo(Math.floor(thisTime/3600));     //时
+      var m = oDateOvideo(Math.floor(thisTime%3600/60));  //分
+      var s = oDateOvideo(Math.floor(thisTime%3600%60));  //秒
+      oUl[1].innerHTML = h+":"+m+":"+s;
+
+      var t = (thisTime/maxTime);
+
+      // console.log(oUl[2].children[0].width);
+      oSp.style.width = parseInt(oUlWidth-2) * t.toFixed(2)+"px";
+      oUl[3].style.left = 134+parseInt( oSp.style.width ) +"px";
+  }
+  oVideo.ontimeupdate = date;
+  oUl[5].onclick = function(){
+      if(oVideo.muted){
+          oVideo.muted=false;
+          oUl[5].innerHTML = "静音";
+      }else{
+          oVideo.muted=true;
+          oUl[5].innerHTML = "声音";
+      }
+  }
+  var move = function(ev){
+      ev = ev || window.event;
+      var _X = ev.clientX;
+      oVideo.ontimeupdate = null;
+      //console.log(_X-oLeft);
+      var t = _X-oLeft;
+      oVideo.currentTime = parseInt(maxTime * (t/oUlWidth).toFixed(2));
+      //console.log(parseInt(oUl[3].style.left));
+      oSp.style.width = oUlWidth * (t/oUlWidth).toFixed(2)+"px";
+      oUl[3].style.left = _X - oSchSp-15 +"px";
+      document.onmousemove = function(ev){
+          ev = ev || window.event;
+          var _x = ev.clientX;
+          var xChange = parseInt(_x)-parseInt(_X);
+
+          //oUl[3].style.left = xChange + _X - oSchSp - 15 +"px";
+
+          var t = _x - oLeft;
+        //   console.log(oVideo.currentTime);
+          oVideo.currentTime = parseInt(maxTime * (t/oUlWidth).toFixed(2));
+          oSp.style.width = oUlWidth * (t/oUlWidth).toFixed(2)+"px";
+          if ((_x - oSchSp-15)<200) {
+              oUl[3].style.left = 200+"px";
+          }
+          else if ((_x - oSchSp-15)>600) {
+              oUl[3].style.left = 600+"px";
+          }
+          else{
+              oUl[3].style.left = _x - oSchSp-15+"px";
+          }
+          //console.log(parseInt(_x)-parseInt(_X));
+          date();
+      }
+      document.onmouseup = function(){
+          //oUl[2].onmousedown = null;
+
+          document.onmousemove = null;
+          oVideo.ontimeupdate = date;
+          //alert(oSp.onmousemove);
+      }
+
+  }
+  oUl[2].onmousedown = move;
+  oUl[3].onmousedown = move;
+  oUl[6].onclick = function(){
+      //oVideo.webkitRequestFullScreen();
+       if(oVideo.requestFullScreen) {
+          oVideo.requestFullScreen();
+        } else if(oVideo.mozRequestFullScreen) {
+          oVideo.mozRequestFullScreen();
+        } else if(oVideo.webkitRequestFullScreen) {
+          oVideo.webkitRequestFullScreen();
+        }
+  }
+}
 })
